8d948275684c80b786ef5406eeffd55fd5f6dac9
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2025-02-25 21:21:16 +0000

    added another sample action and store in the store.js for practice

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-export const initialStore=()=>{
-  return{
+export const initialStore = () => {
+  return {
     message: null,
     todos: [
       {
@@ -12,21 +12,35 @@ export const initialStore=()=>{
         title: "Do my homework",
         background: null,
       }
+    ],
+    sampleStoreNames: [
+      {
+        prop1: 5,
+        prop2: "random value here",
+      }
     ]
   }
 }
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
+  switch (action.type) {
+
     case 'add_task':
-
-      const { id,  color } = action.payload
-
+      const { id, color } = action.payload
       return {
         ...store,
         todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
       };
+
+    case 'actionNameHere':
+      const { value1, value2 } = action.payload /* payload here is the object holding your props */
+      return {
+        ...store,
+        /* store name here*/sampleStoreNames: store.sampleStoreNames.map((sampleStoreName) => (sampleStoreName.prop1 === value1 ? { ...sampleStoreName, prop2: value2 } : sampleStoreName))
+      };
+
     default:
       throw Error('Unknown action.');
-  }    
+  }
 }
+
